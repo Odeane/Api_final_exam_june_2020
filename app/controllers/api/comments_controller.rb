3941968e7b883ff3932250ec98729a8f192f3975
odeane_croney <odeane_croney@yahoo.com>
2020-09-09 13:50:42 +0200

    adding association and trying to fetch user comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,15 @@
 class Api::CommentsController < ApplicationController
+  before_action :authenticate_user! 
+
   def create
+    comment = current_user.comments.create(comment_params)
     binding.pry
   end
+end
+
+
+private
+
+def comment_params
+params.require(:comment).permit(:body, :article_id, :user_id)
 end
