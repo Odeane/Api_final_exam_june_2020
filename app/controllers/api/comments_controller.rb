80d56023af6dc65db6947d90b972e08d015b5748
odeane_croney <odeane_croney@yahoo.com>
2020-09-09 14:01:21 +0200

    rendering message for sucessfully creating article.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 class Api::CommentsController < ApplicationController
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
 
   def create
     comment = current_user.comments.create(comment_params)
-    binding.pry
+    render json: {message: "Comment successfully created"}
   end
 end
-
 
 private
 
 def comment_params
-params.require(:comment).permit(:body, :article_id, :user_id)
+  params.require(:comment).permit(:body, :article_id, :user_id)
 end
